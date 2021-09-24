96ccb66d4b3d2fed349ceacaeac316f55b67423c
mcrutch <51446717+mcrutch@users.noreply.github.com>
2021-09-24 16:04:15 +0000

    Add support for injecting the UUID and git commit id of the notebook into the environment

---
+++ inject-uuid/src/index.ts
@@ -0,0 +1,42 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+
+import {
+  NotebookPanel, INotebookTracker
+} from '@jupyterlab/notebook';
+
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: "@jupyterlab-nbgallery/inject-uuid",
+  autoStart: true,
+  requires: [INotebookTracker],
+  activate: (
+    app: JupyterFrontEnd,
+    notebooks: INotebookTracker
+  ) => {
+    notebooks.forEach(injectUUID)
+    notebooks.widgetAdded.connect((_, a) => injectUUID(a))
+  },
+};
+function injectUUID(panel: NotebookPanel): void {
+  panel.sessionContext.ready.then(() => {
+      let gallery_metadata :any;
+      gallery_metadata = panel.model.metadata.toJSON()["gallery"];
+      if(gallery_metadata && gallery_metadata['uuid']){
+        let kernel = panel.sessionContext.session.kernel;
+        console.log(kernel);
+        if(kernel.name == "python" || kernel.name == "python3"){
+          kernel.requestExecute({code:"import os; os.environ['NBGALLERY_UUID']='"+gallery_metadata['uuid']+"'; os.environ['NBGALLERY_GIT_COMMIT_ID']='"+gallery_metadata['git_commit_id']+"';",silent: true,stop_on_error: true});
+        }
+        if(kernel.name == "ruby"){
+          kernel.requestExecute({code:"ENV['NBGALLERY_UUID']='"+gallery_metadata['uuid']+"'",silent: true,stop_on_error: true});
+          kernel.requestExecute({code:"ENV['NBGALLERY_GIT_COMMIT_ID']='"+gallery_metadata['git_commit_id']+"'",silent: true,stop_on_error: true});
+        }
+      }
+  });
+}
+
+export default extension;
