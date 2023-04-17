eef877a1c597d8993410da840f2b90c5b0686dc0
mcrutch <51446717+mcrutch@users.noreply.github.com>
2023-04-17 10:25:20 -0400

    Fix deps (#32)
    * Trying to fix dependencies
    
    * Post build 1
    
    * Closes #17
    
    * Adding lock and one last cleanup of deps

--- inject-uuid/src/index.ts
+++ inject-uuid/src/index.ts
@@ -21,20 +21,40 @@ const extension: JupyterFrontEndPlugin<void> = {
     notebooks.widgetAdded.connect((_, a) => injectUUID(a))
   },
 };
+function inject(panel: NotebookPanel, gallery_metadata: any): void{
+  let kernel = panel.sessionContext.session.kernel;
+  console.log(kernel);
+  if (kernel.name == "python" || kernel.name == "python3") {
+    kernel.requestExecute({ code: "import os; os.environ['NBGALLERY_UUID']='" + gallery_metadata['uuid'] + "'; os.environ['NBGALLERY_GIT_COMMIT_ID']='" + gallery_metadata['git_commit_id'] + "';", silent: true, stop_on_error: true });
+  }
+  if (kernel.name == "ruby") {
+    kernel.requestExecute({ code: "ENV['NBGALLERY_UUID']='" + gallery_metadata['uuid'] + "'", silent: true, stop_on_error: true });
+    kernel.requestExecute({ code: "ENV['NBGALLERY_GIT_COMMIT_ID']='" + gallery_metadata['git_commit_id'] + "'", silent: true, stop_on_error: true });
+  }
+}
 function injectUUID(panel: NotebookPanel): void {
   panel.sessionContext.ready.then(() => {
     let gallery_metadata: any;
+    let restarting = false;
+    let unknown = false;
     gallery_metadata = panel.model.sharedModel.metadata["gallery"];
     if (gallery_metadata && gallery_metadata['uuid']) {
-      let kernel = panel.sessionContext.session.kernel;
-      console.log(kernel);
-      if (kernel.name == "python" || kernel.name == "python3") {
-        kernel.requestExecute({ code: "import os; os.environ['NBGALLERY_UUID']='" + gallery_metadata['uuid'] + "'; os.environ['NBGALLERY_GIT_COMMIT_ID']='" + gallery_metadata['git_commit_id'] + "';", silent: true, stop_on_error: true });
-      }
-      if (kernel.name == "ruby") {
-        kernel.requestExecute({ code: "ENV['NBGALLERY_UUID']='" + gallery_metadata['uuid'] + "'", silent: true, stop_on_error: true });
-        kernel.requestExecute({ code: "ENV['NBGALLERY_GIT_COMMIT_ID']='" + gallery_metadata['git_commit_id'] + "'", silent: true, stop_on_error: true });
-      }
+      inject(panel, gallery_metadata);
+      panel.sessionContext.statusChanged.connect(() =>{
+        if(panel.sessionContext.session.kernel.status == "restarting"){
+          restarting=true;
+        }
+        if(panel.sessionContext.session.kernel.status == "unknown" && restarting){
+          // Trying to win the race on a restart kernel-> run all cells
+          inject(panel, gallery_metadata);
+          unknown=true;
+        }
+        if((panel.sessionContext.session.kernel.status == "idle" || panel.sessionContext.session.kernel.status == "busy") && restarting && unknown){
+          restarting = unknown = false;
+          // Fail safe
+          inject(panel, gallery_metadata);
+        }
+      });
     }
   });
 }
