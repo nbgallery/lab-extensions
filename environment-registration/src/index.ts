751f5afd3817db4d84511f745379c318c696022d
mcrutch <51446717+mcrutch@users.noreply.github.com>
2023-04-27 10:59:37 +0000

    Improve environment registration behavior - Register Notebook if using Notebookv7 - Use settings file instead of environment variables if present

--- environment-registration/src/index.ts
+++ environment-registration/src/index.ts
@@ -28,23 +28,31 @@ const extension: JupyterFrontEndPlugin<void> = {
     let registered = false;
     let nbgallery_url = "";
     let nbgallery_client_name = "";
-
+    let env_nbgallery_url = "";
+    let env_nbgallery_client_name = "";
     function get_url() {
       return PageConfig.getBaseUrl();
     }
     function loadSetting(setting: ISettingRegistry.ISettings): void {
       // Read the settings and convert to the correct type
-      if (nbgallery_url && nbgallery_url.length > 0) {
+      nbgallery_url = setting.get('nbgallery_url').composite as string;
+      if ((!nbgallery_url || nbgallery_url.length == 0) && env_nbgallery_url && env_nbgallery_url.length > 0) {
+        nbgallery_url = env_nbgallery_url;
         setting.set("nbgallery_url", nbgallery_url);
-      } else {
-        nbgallery_url = setting.get('nbgallery_url').composite as string;
       }
-      if (nbgallery_client_name && nbgallery_client_name.length > 0) {
+
+      nbgallery_client_name = setting.get('nbgallery_client_name').composite as string;
+      if ((!nbgallery_client_name || nbgallery_client_name.length == 0) && env_nbgallery_client_name && env_nbgallery_client_name.length > 0) {
+        nbgallery_client_name = env_nbgallery_client_name;
         setting.set("nbgallery_client_name", nbgallery_client_name);
-      } else {
-        nbgallery_client_name = setting.get('nbgallery_client_name').composite as string;
       }
       registered = setting.get('registered').composite as boolean;
+
+      const page = PageConfig.getOption("notebookPage");
+      let user_interface = 'lab';
+      if( page != ""){
+        user_interface = 'notebook';
+      }
 
       if (!registered && nbgallery_url && nbgallery_url.length > 0) {
         $.ajax({
@@ -54,21 +62,20 @@ const extension: JupyterFrontEndPlugin<void> = {
           data: {
             name: nbgallery_client_name,
             url: get_url(),
-            user_interface: "lab"
+            user_interface: user_interface
           },
           xhrFields: { withCredentials: true },
           success: function (data) {
             setting.set("registered", true);
             console.log("Environment Registered to NBGallery");
-
           },
         });
       }
     }
     try {
       const data = await requestAPI<any>('environment');
-      nbgallery_url = data['NBGALLERY_URL'];
-      nbgallery_client_name = data['NBGALLERY_CLIENT_NAME'];
+      env_nbgallery_url = data['NBGALLERY_URL'];
+      env_nbgallery_client_name = data['NBGALLERY_CLIENT_NAME'];
     } catch (reason) {
       console.error(`ERROR on get /jupyter_nbgallery/environment.\n ${reason}`);
     }
